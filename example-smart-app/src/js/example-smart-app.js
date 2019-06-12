105a32c453d971453825cfee10ec55b974d0d6db
dfear2112 <51330992+dfear2112@users.noreply.github.com>
2019-06-12 12:04:15 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -53,14 +53,14 @@
             lname = patient.name[0].family;
           }
 
-          // Observations
-          //lymph = byCodes('26478-8');
+           Observations
+          lymph = byCodes('26478-8');
           // Cerner SoF Tutorial Observations
-          // var height = byCodes('8302-2');
-          // var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          // var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          // var hdl = byCodes('2085-9');
-          // var ldl = byCodes('2089-1');
+           var height = byCodes('8302-2');
+           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
+           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+           var hdl = byCodes('2085-9');
+           var ldl = byCodes('2089-1');
 
 
           var p = defaultPatient();
