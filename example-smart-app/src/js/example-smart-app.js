736b06308e21383833aff3804385123f5680a00f
dfear2112 <51330992+dfear2112@users.noreply.github.com>
2019-06-12 13:17:18 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -12,16 +12,16 @@
         var patient = smart.patient;
         var pt = patient.read();
 
-        var obv = smart.patient.api.fetchAll({
-           type: 'Observation',
-           query: {
-             code: {
-               $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                     'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                     'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-             }
-           }
-         });
+        //var obv = smart.patient.api.fetchAll({
+         //  type: 'Observation',
+          // query: {
+           //  code: {
+             //  $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+               //      'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+                  //   'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+       //      }
+        //   }
+        // });
 
         var obv = smart.patient.api.fetchAll({
           type: 'Observation',
@@ -56,9 +56,9 @@
            //Observations
           lymph = byCodes('26478-8');
           // Cerner SoF Tutorial Observations
-           var height = byCodes('8302-2');
-           var systolicbp = getBloodPressureValue(byCodes'8480-6');
-           var diastolicbp = getBloodPressureValue(byCodes'8462-4');
+           //var height = byCodes('8302-2');
+          // var systolicbp = getBloodPressureValue(byCodes'8480-6');
+          // var diastolicbp = getBloodPressureValue(byCodes'8462-4');
           // var hdl = byCodes('2085-9');
            //var ldl = byCodes('2089-1');
 
@@ -112,13 +112,10 @@
       gender: {value: ''},
       birthdate: {value: ''},
       lymph: {value: ''},
-      systolicbp: {value: ''},
-      diastolicbp: {value: ''},
-      ldl: {value:''},
-      hdl: {value: ''}
+     
 
       // Cerner SoF Tutorial Observations
-       height: {value: ''},
+      // height: {value: ''},
       // systolicbp: {value: ''},
       // diastolicbp: {value: ''},
       // ldl: {value: ''},
@@ -168,8 +165,8 @@
     // Cerner SoF Tutorial Observations
 
     // $('#height').html(p.height);
-     $('#systolicbp').html(p.systolicbp);
-     $('#diastolicbp').html(p.diastolicbp);
+    // $('#systolicbp').html(p.systolicbp);
+    // $('#diastolicbp').html(p.diastolicbp);
     // $('#ldl').html(p.ldl);
     // $('#hdl').html(p.hdl);
   };
