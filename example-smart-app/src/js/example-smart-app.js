43b67b0edc7eb08ecfb7139c9182c8baa4894a97
dfear2112 <51330992+dfear2112@users.noreply.github.com>
2019-07-15 14:33:17 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -65,7 +65,7 @@
           
           //FamilyMemberHistory
          // smart.patient.api.search({type: 'FamilyMemberHistory'});
-           var family = smart.patient.api.fetchAll({type: 'FamilyMemberHistory'});
+           var family = smart.patient.api.search({type: 'FamilyMemberHistory'});
             family.done(function(patient){
             console.log(patient);
               });
