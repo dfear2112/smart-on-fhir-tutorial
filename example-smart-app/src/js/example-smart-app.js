0574878937c87ec5470727e4ddf1ca98cc0fdcb2
dfear2112 <51330992+dfear2112@users.noreply.github.com>
2019-07-15 16:02:51 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -67,12 +67,13 @@
          //smart.patient.api.search({type: 'FamilyMemberHistory'});
     
           //smart.api.read({type: resourceType, id: resourceId})
-          var family = smart.patient.api.read({type: 'FamilyMemberHistory', id: "resourceId"});
-            $.when(pt, family).fail(onError);
-            $.when(pt, family).done(function(patient, family){
-            console.log(patient);
-              });
-
+          //var family = smart.patient.api.read({type: 'FamilyMemberHistory', id: "resourceId"});
+          //  $.when(pt, family).fail(onError);
+           // $.when(pt, family).done(function(patient, family){
+           // console.log(patient);
+            //  });
+            var family = smart.patient.api.search({type: 'FamilyMemberHistory'});
+          
 
 
           var p = defaultPatient();
