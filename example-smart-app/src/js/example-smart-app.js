f0698bf688ed6ba7e2e60469b61b4d0ee1d39d36
dfear2112 <dfear@som.geisinger.edu>
2019-07-16 16:48:18 -0400

    text

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -46,7 +46,7 @@
             // $.when(pt, family).fail(onError);
             // $.when(pt, family).done(function(patient, family) {
             // var family = smart.patient.FamilyMemberHistory;
-            var family = smart.patient.api.read((type: 'FamilyMemberHistory', id: "SMART-FamilyMemberHistory-14"));
+            var family = smart.patient.api.read((type: 'FamilyMemberHistory', id: 'SMART-FamilyMemberHistory-14'));
 
 
 
