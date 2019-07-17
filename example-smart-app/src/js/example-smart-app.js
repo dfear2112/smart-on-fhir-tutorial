e1164038c983735e308927742763a889e7d62413
dfear2112 <dfear@som.geisinger.edu>
2019-07-17 15:21:16 -0400

    testing observation

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -10,19 +10,12 @@
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
-// serum glucose LOINC was found to be 2345-7
-// bmi LOINC is 39156-5
+//SMOMED 44054006 codes for Type 2 Diabetes
         var obv = smart.patient.api.fetchAll({
-          type: 'Observation',
+          type: 'Condition',
           query: {
             code: {
-              $or: ['http://loinc.org|2345-7',
-                    'http://loinc.org|8302-2',
-                    'http://loinc.org|55284-4',
-                    'http://loinc.org|8480-6',
-                    'http://loinc.org|8462-4',
-                    'http://loinc.org|2345-7',
-                    'http://loinc.org|39156-5',
+              $or: ['http://snomed.info/sct|44054006',
                    ]
             }
           }
@@ -36,7 +29,7 @@
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           console.log("byCodes:");
-          console.log(byCodes('2345-7'));
+          console.log(byCodes('44054006'));
 
 
           var gender = patient.gender;
@@ -48,32 +41,14 @@
           }
 
 
-          // Observations
-          height = byCodes('8302-2');
-          console.log(height);
-          systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          serum_glucose = byCodes('2345-7');
-          bmi = byCodes('39156-5');
-
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
           p.gender = gender;
           p.fname = fname;
           p.lname = lname;
 
-          // Observations
-          p.height = getQuantityValueAndUnit(height[0]);
-          p.serum_glucose = getQuantityValueAndUnit(serum_glucose[0]);
-          p.bmi = getQuantityValueAndUnit(bmi[0]);
-
-
-           if (typeof systolicbp != 'undefined')  {
-             p.systolicbp = systolicbp;
-           }
-           if (typeof diastolicbp != 'undefined') {
-             p.diastolicbp = diastolicbp;
-           }
+          //Conditions
+          p.condition = getQuantityValueAndUnit(condition[0]);
 
           console.log('p:');
           console.log(p);
@@ -92,29 +67,11 @@
       lname: {value: ''},
       gender: {value: ''},
       birthdate: {value: ''},
-      height: {value: ''},
-      systolicbp: {value: ''},
-      diastolicbp: {value: ''},
-      serum_glucose: {value: ''},
-      bmi: {value: ''},
+      condition: {value: ''},
     };
   }
   // Helper Function
-  function getBloodPressureValue(BPObservations, typeOfPressure) {
-    var formattedBPObservations = [];
-    BPObservations.forEach(function(observation){
-      var BP = observation.component.find(function(component){
-        return component.code.coding.find(function(coding) {
-          return coding.code == typeOfPressure;
-        });
-      });
-      if (BP) {
-        observation.valueQuantity = BP.valueQuantity;
-        formattedBPObservations.push(observation);
-      }
-    });
-    return getQuantityValueAndUnit(formattedBPObservations[0]);
-  }
+
   function getQuantityValueAndUnit(ob) {
     if (typeof ob != 'undefined' &&
         typeof ob.valueQuantity != 'undefined' &&
@@ -132,10 +89,6 @@
     $('#lname').html(p.lname);
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
-    $('#height').html(p.height);
-    $('#systolicbp').html(p.systolicbp);
-    $('#diastolicbp').html(p.diastolicbp);
-    $('#serum_glucose').html(p.serum_glucose);
-    $('#bmi').html(p.bmi);
+    $('#condition').html(p.condition);
   };
 })(window);
