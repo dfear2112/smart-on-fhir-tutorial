cbb84f4232539be2c0f984a2c1518375f7c4132c
dfear2112 <dfear@som.geisinger.edu>
2019-07-22 17:10:03 -0400

    test

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -238,7 +238,7 @@
           console.log(jsonResponse);
           var i;
           var genes = '';
-          for (i =0; i <= jsonResponse.length; i++) {
+          for (i =0; i <= jsonResponse.associations.length; i++) {
             genes += jsonResponse.associations[i].subject.label + "<br>";
 
           };
