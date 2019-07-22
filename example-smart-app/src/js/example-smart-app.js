067f547c838eb3c953131883428007795c9710c5
dfear2112 <dfear@som.geisinger.edu>
2019-07-22 14:13:47 -0400

    test

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -232,7 +232,7 @@
           var xmlHttp = new XMLHttpRequest();
           xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
           xmlHttp.send( null );
-          return xmlHttp.responseText.id.label;
+          return xmlHttp.responseText.label;
           console.log(xmlHttp.responseText);
       }
   window.drawVisualization = function(p) {
